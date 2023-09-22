2a144009db4ce905498f41721494c8197119f17d
SholpaNur <115505255+SholpaNur@users.noreply.github.com>
2023-09-22 14:28:28 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
-function sayHello() {
-alert('hello world!')   
-}
+// STEP 1
+/* let some_month          let someMonth
+function the Month()    function theMonth()
+let current-month       let currentMonth
+let summer_month        let summerMonth
+let MyLibrary-function  let MyLibrary() */
+// STEP 2
+
+// STEP 3
+// STEP 4
+// STEP 5
+// STEP 6
+// STEP 7
