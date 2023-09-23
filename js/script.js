18d9fdd264c84966e0dde13c462a7f37573a8d45
Sholpan Nurgalieva <s11041978@gmail.com>
2023-09-22 22:39:59 -0700

    complete

---
+++ js/script.js
@@ -0,0 +1,77 @@
+// STEP 1
+/* let some_month          let someMonth
+function the Month()    function theMonth()
+let current-month       let currentMonth
+let summer_month        let summerMonth
+let MyLibrary-function  let myLibraryFunction */
+// STEP 2
+/*let numericLiteral = 42; let stringLiteral = "Hello, World!"; let booleanLiteral = true; let nullLiteral = null; */ 
+// STEP 3
+/*let x = 5; let y = 10; let result = x * y + (x - 1); //arithmetic
+let age = 3; let schoolAge = age >= 5 ? "Yes" : "No"; //ternary */
+// STEP 4
+/* let strFirstName;
+let strLastName;
+let strAddress;
+let strCity;
+let strState;
+let strZipCode;
+let intYourAge;
+let strReferralSource;
+let boolMayWeContactYou; */
+ //STEP 5
+/*let strFirstName; strFirstName = "Jane";
+let strLastName; strLastName = "Smith";
+let intYourAge; intYourAge = 21;
+
+let strFirstName = "Mary";
+let strLastName = "Johnson";
+let intYourAge = 30;
+
+let strFirstName = "Alice", strLastName = "Jones", intYourAge = 45; */
+// STEP 6
+/* let variable1; variable1 = 5 + " age"; console.log(variable1); */
+// STEP 7
+/*let variable2 = true + " is boolean"; console.log(variable2);
+let variable3 = 10 + true; console.log(variable3); */
+// STEP 8
+/*let someString = ' Who once said, "Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former." ';
+console.log(someString) // fix I\'m */
+// STEP 9
+/*let nullValue = null; console.log(nullValue);
+let undefinedValue; console.log(undefinedValue); */
+// STEP 10
+/* console.log(typeof "hello"); 
+console.log(typeof 10); 
+console.log(typeof false); 
+console.log(typeof { object: "value" }); 
+console.log(typeof undefinedValue); */
+// STEP 11
+/* let firstName = "Sholpan"; let lastName = "Nurgalieva"; alert("Hello " + firstName + " " + lastName + ", welcome to the JavaScript class!"); */
+// STEP 12
+/* let Name = "Sholpan"; alert("Hello " + Name + ", welcome to the JavaScript class!"); */
+// STEP 13
+/* let course = "JavaScript"; let firstName = "Sholpan"; let lastName = "Nurgalieva";
+alert("Hello " + firstName + " " + lastName + ", welcome to the " + course + " class!"); */
+// STEP 14
+/*let course = "JavaScript"; let firstName = "Sholpan"; let lastName = "Nurgalieva";
+alert("Hello " + firstName + " " + lastName + ". \nWelcome to the " + course + " class!"); */
+// STEP 15
+/* let course = "JavaScript"; let name = prompt("Please enter your name:");
+alert("Hello " + name + ".\nWelcome to the " + course + " class!"); */
+// STEP 16
+/* let name = prompt("Please enter your name:"); let course = prompt("Please enter your course:");
+alert("Hello " + name + ".\nWelcome to the " + course + " class!"); */
+// STEP 17
+/* let x = 11; let y = 21; let sum = x + y; console.log(sum); */
+// STEP 18
+/* let x = 20; x += 20; console.log(x); */
+// STEP 19
+/* let x = 20; x *= 5; console.log(x); */
+// STEP 20
+/* let x=20%3; x /= 1; console.log(x); */
+// STEP 21
+/* let a = 1; let b = 2; let c = 3; let result = a < b && b < c; console.log(result); */
+// STEP 22
+/* let x = 1; let y = 2; let result = x > 2 || y < 1; console.log(result); */
+
