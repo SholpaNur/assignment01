d57851e02ee28a05ed0bf85f0574e75ecc0e9512
SholpaNur <115505255+SholpaNur@users.noreply.github.com>
2023-09-22 21:22:25 -0700

    Update script.js

--- script.js
+++ script.js
@@ -3,11 +3,32 @@
 function the Month()    function theMonth()
 let current-month       let currentMonth
 let summer_month        let summerMonth
-let MyLibrary-function  let MyLibrary() */
+let MyLibrary-function  let MyLibraryFunction */
 // STEP 2
-
+/*let numericLiteral = 42; let stringLiteral = "Hello, World!"; let booleanLiteral = true; let nullLiteral = null; */ 
 // STEP 3
+/*let x = 5; let y = 10; let result = x * y + (x - 1); //arithmetic
+let age = 3; let schoolAge = age >= 5 ? "Yes" : "No"; //ternary */
 // STEP 4
-// STEP 5
+/* let strFirstName;
+let strLastName;
+let strAddress;
+let strCity;
+let strState;
+let strZipCode;
+let intYourAge;
+let strReferralSource;
+let boolMayWeContactYou; */
+ //STEP 5
+/*let strFirstName; strFirstName = "Jane";
+let strLastName; strLastName = "Smith";
+let intYourAge; intYourAge = 21;
+
+let strFirstName = "Mary";
+let strLastName = "Johnson";
+let intYourAge = 30;
+
+let strFirstName = "Alice", strLastName = "Jones", intYourAge = 45; */
 // STEP 6
+/*let oneVariable; oneVariable = 5 + " age"; console.log(oneVariable); */
 // STEP 7
